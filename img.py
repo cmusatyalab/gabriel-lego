598128112bf269aff09040d4afe0119450d3d39a
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-04-23 21:21:26 -0400

    Can now display contours

--- img.py
+++ img.py
@@ -6,6 +6,7 @@
 import cv2
 import sys
 import time
+import math
 import lego_cv as lc
 import numpy as np
 
@@ -18,8 +19,33 @@ black_max = np.array([100, 100, 100], np.uint8)
 mask_black = cv2.inRange(img, black_min, black_max)
 img_black = cv2.bitwise_and(img, img, mask = mask_black)
 cv2.namedWindow("black")
-lc.display_image("black", img_black)
+lc.display_image("black", mask_black)
 
+mask_black_copy = mask_black.copy()
+contours, hierarchy = cv2.findContours(mask_black_copy, mode = cv2.RETR_LIST, method = cv2.CHAIN_APPROX_NONE )
+mask_black_dots = mask_black.copy()
+counts = np.zeros((9, 16))
+for contour in contours:
+    contour.resize(len(contour), 2)
+    if len(contour) > 20:
+        lc.set_value(mask_black_dots, contour, 0)
+        #lc.display_image("black", mask_black_dots)
+        continue
+    max_p = contour.max(axis = 0)
+    min_p = contour.min(axis = 0)
+    #print "max: %s, min: %s" % (max_p, min_p)
+    diff_p = max_p - min_p
+    if diff_p.max() > 5:
+        lc.set_value(mask_black_dots, contour, 0)
+        continue
+    mean_p = contour.mean(axis = 0)
+    counts[int(mean_p[1] / 80), int(mean_p[0] / 80)] += 1
+
+cv2.namedWindow("black_dots")
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+lc.display_image("black_dots", img)
+
+print counts
 
 try:
     while True:
