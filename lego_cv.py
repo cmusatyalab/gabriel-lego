fe61e01037f732aec0c89bd94f561cb5ff12a045
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-04-26 17:33:49 -0400

    Can do perspective correction for sample images!

--- lego_cv.py
+++ lego_cv.py
@@ -64,6 +64,8 @@ def ind2sub(size, idx):
     return (idx / size[1], idx % size[1])
 
 def euc_dist(p1, p2):
+    p1 = np.array(p1)
+    p2 = np.array(p2)
     return np.linalg.norm(p1 - p2)
 
 def is_roughly_convex(cnt, threshold = 0.7):
@@ -71,6 +73,74 @@ def is_roughly_convex(cnt, threshold = 0.7):
     hull_area = cv2.contourArea(hull)
     cnt_area = cv2.contourArea(cnt)
     return (float(cnt_area) / hull_area > threshold)
+
+def is_line_seg_close(line1, line2):
+    pt1_1 = np.array(line1[0 : 2])
+    pt1_2 = np.array(line1[2 : 4])
+    pt2_1 = np.array(line2[0 : 2])
+    pt2_2 = np.array(line2[2 : 4])
+    l1 = euc_dist(pt1_1, pt1_2)
+    l2 = euc_dist(pt2_1, pt2_2)
+    v1 = pt1_2 - pt1_1
+    v2 = pt2_1 - pt1_1
+    v3 = pt2_2 - pt1_1
+    area1 = np.absolute(np.cross(v1, v2))
+    area2 = np.absolute(np.cross(v1, v3))
+    if max(area1, area2) < l1 * l2 / 3:
+        return True
+    else:
+        return False
+
+def line_interset(a, b):
+    x1 = a[0]; y1 = a[1]; x2 = a[2]; y2 = a[3]
+    x3 = b[0]; y3 = b[1]; x4 = b[2]; y4 = b[3]
+    d = ((float)(x1-x2) * (y3-y4)) - ((y1-y2) * (x3-x4))
+    if d:
+        x = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / d;
+        y = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / d;
+    else:
+        x, y = (-1, -1)
+    return (x, y)
+
+def get_corner_pts(bw):
+    lines = cv2.HoughLinesP(bw, 1, np.pi/180, 50, minLineLength = 100, maxLineGap = 100)
+    lines = lines[0]
+    new_lines = list()
+    for line in lines:
+        flag = True
+        for new_line in new_lines:
+            if is_line_seg_close(line, new_line):
+                flag = False
+                break
+        if flag:
+            new_lines.append(list(line))
+
+    mean_p = lines.mean(axis = 0)
+    mean_p = (np.mean([mean_p[0], mean_p[2]]), np.mean([mean_p[1], mean_p[3]]))
+    corners = list()
+    for idx1, line1 in enumerate(new_lines):
+        for idx2, line2 in enumerate(new_lines):
+            if idx1 >= idx2:
+                continue
+            inter_p = line_interset(line1, line2)
+            dist = euc_dist(inter_p, mean_p)
+            if dist < 500:
+                corners.append(inter_p)
+
+    dtype = [('x', float), ('y', float)]
+    corners = np.array(corners, dtype = dtype)
+    corners = np.sort(corners, order = 'y')
+    if corners[0][0] < corners[1][0]:
+        ul = corners[0]; ur = corners[1]
+    else:
+        ul = corners[1]; ur = corners[0]
+    if corners[2][0] < corners[3][0]:
+        bl = corners[2]; br = corners[3]
+    else:
+        bl = corners[3]; br = corners[2]
+
+    corners = np.float32([list(ul), list(ur), list(bl), list(br)])
+    return corners
 
 
 ##################### Below are only for the Lego task #########################
@@ -130,6 +200,12 @@ def locate_lego(img):
     #approx = cv2.approxPolyDP(hull, 100, True)
     mask_board = np.zeros(mask_black.shape, dtype=np.uint8)
     cv2.drawContours(mask_board, [hull], 0, 255, -1)
+    
+    board_border = np.zeros(mask_black.shape, dtype=np.uint8)
+    cv2.drawContours(board_border, [hull], 0, 255, 1)
+    corners = get_corner_pts(board_border)
+    target_points = np.float32([[0, 0], [270, 0], [0, 155], [270, 155]])
+    perspective_mtx = cv2.getPerspectiveTransform(corners, target_points)
 
     board_area = cv2.contourArea(hull)
     M = cv2.moments(hull)
@@ -141,7 +217,8 @@ def locate_lego(img):
         #cv2.drawContours(img, [hull], 0, (0, 255,0), 3)
         img_board = cv2.bitwise_and(img, img, mask = mask_board)
     bw_board = cv2.cvtColor(img_board, cv2.COLOR_BGR2GRAY)
-    display_image("board", img_board)
+    dst = cv2.warpPerspective(img_board, perspective_mtx, (1280,720))
+    display_image("board", dst)
 
     edges = cv2.Canny(bw_board, 100, 200)
     display_image("edge", edges)
