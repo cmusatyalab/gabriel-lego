e494a27b20cd10a6517fb34a1c04d4dfea6f2979
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-04-28 19:31:00 -0400

    Added lego config file

---
+++ lego_config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Cloudlet Infrastructure for Mobile Computing
+#   - Task Assistance
+#
+#   Author: Zhuo Chen <zhuoc@cs.cmu.edu>
+#
+#   Copyright (C) 2011-2013 Carnegie Mellon University
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+BOARD_RECONSTRUCT_HEIGHT = 155
+BOARD_RECONSTRUCT_WIDTH = 270
+
+BRICK_HEIGHT = BOARD_RECONSTRUCT_HEIGHT / 12.5
+BRICK_WIDTH = BOARD_RECONSTRUCT_WIDTH / 26.2
+
