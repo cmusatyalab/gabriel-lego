57786f1e627b7326edc165d6cc2015854f25ef8c
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-06-26 20:04:01 -0400

    Initial file for a simple task

---
+++ tasks/task_Turtle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import numpy as np
+
+# Labels: nothing:0, white:1, green:2, yellow:3, red:4, blue:5, black:6, unsure:7
+bitmaps = [np.array([[2, 2, 2, 2]]),
+           np.array([[0, 0, 1, 0],
+                     [2, 2, 2, 2]]),
+           np.array([[0, 0, 1, 6],
+                     [2, 2, 2, 2]]),
+           np.array([[0, 0, 1, 1],
+                     [0, 0, 1, 6],
+                     [2, 2, 2, 2]]),
+           np.array([[0, 0, 1, 1],
+                     [0, 2, 1, 6],
+                     [2, 2, 2, 2]]),
+           np.array([[0, 2, 1, 1],
+                     [0, 2, 1, 6],
+                     [2, 2, 2, 2]]),
+           np.array([[0, 2, 2, 2],
+                     [0, 2, 1, 1],
+                     [0, 2, 1, 6],
+                     [2, 2, 2, 2]]),
+           ]
