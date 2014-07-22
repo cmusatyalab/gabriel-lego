5d1acd7ed2ef36278db25d78afd06f1e65f0ba7c
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-07-21 20:14:05 -0400

    Added client code for Lego task assistance. Similar to Gabriel client

---
+++ client/src/edu/cmu/cs/gabriel/Const.java
@@ -0,0 +1,35 @@
+package edu.cmu.cs.gabriel;
+
+import java.io.File;
+
+import android.os.Environment;
+
+public class Const {
+	/* 
+	 * Experiement variable
+	 */
+	
+	public static final boolean IS_EXPERIMENT = false;
+	
+	// Transfer from the file list
+	// If TEST_IMAGE_DIR is not none, transmit from the image
+	public static File ROOT_DIR = new File(Environment.getExternalStorageDirectory() + File.separator + "Gabriel" + File.separator);
+	public static File TEST_IMAGE_DIR = null; //new File (ROOT_DIR.getAbsolutePath() + File.separator + "images" + File.separator);	
+	
+	// control VM
+	public static String GABRIEL_IP = "128.2.213.103";	// Cloudlet
+//	public static String GABRIEL_IP = "54.203.73.67";	// Amazon West
+	
+	// Token
+	public static int MAX_TOKEN_SIZE = 1;
+	
+	// image size and frame rate
+	public static int MIN_FPS = 5;
+	public static int IMAGE_WIDTH = 640;
+	public static int IMAGE_HEIGHT = 360;
+
+	// Result File
+	public static String LATENCY_FILE_NAME = "latency-" + GABRIEL_IP + "-" + MAX_TOKEN_SIZE + ".txt";
+	public static File LATENCY_DIR = new File(ROOT_DIR.getAbsolutePath() + File.separator + "exp");
+	public static File LATENCY_FILE = new File (LATENCY_DIR.getAbsolutePath() + File.separator + LATENCY_FILE_NAME);
+}
