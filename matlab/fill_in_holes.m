26bada148c2a2dc16a6666111c5b934d2d77a774
Zhuo Chen <zhuoc@cs.cmu.edu>
2017-10-19 15:47:23 +0000

    Clean repo

--- matlab/fill_in_holes.m
+++
@@ -1,8 +0,0 @@
-function [ new_bw ] = fill_in_holes( original, size )
-filled = imfill(original, 'holes');
-holes = filled & ~original;
-bigholes = bwareaopen(holes, size);
-smallholes = holes & ~bigholes;
-new_bw = original | smallholes;
-end
-
--- matlab/get_transform_matrix.m
+++
@@ -1,9 +0,0 @@
-function [ tform ] = get_transform_matrix( mask_board )
-% input_points = [282, 462; 644, 410; 269, 671; 707, 591]; % im30
-input_points = [271, 491; 660, 447; 232, 734; 719, 651]; % im42
-base_points = [0, 0; 270, 0; 0, 155; 270, 155];
-
-tform = cp2tform(input_points, base_points, 'projective');
-
-end
-
