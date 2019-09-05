1e0d154884d9ecb333617639d4a4ba79d8b6fbda
Manuel Olguin <manuel@olguin.se>
2019-09-05 15:25:56 -0400

    add tests for bitmap differences

--- tests/cv_test.py
+++ tests/cv_test.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import Dict
 
 import numpy as np
 
@@ -9,8 +10,8 @@ from cv.image_util import ImageProcessError, preprocess_img
 class CVTest(unittest.TestCase):
     correct_state = np.array([[0, 0, 1, 6],
                               [2, 2, 2, 2]])
-    incorrect_state = np.array([[0, 2, 1, 1],
-                                [0, 2, 1, 6],
+    incorrect_state = np.array([[0, 0, 1, 1],
+                                [0, 0, 1, 6],
                                 [2, 2, 2, 2]])
 
     def setUp(self) -> None:
@@ -33,3 +34,26 @@ class CVTest(unittest.TestCase):
 
         with self.assertRaises(expected_exception=ImageProcessError) as e:
             bitmap_bad = preprocess_img(cv_img_bad)
+
+    def test_bitmaps_diff(self):
+        self.assertIsNone(bm.bitmap_diff(self.correct_state,
+                                         self.correct_state.copy()))
+        self.assertIsNone(bm.bitmap_diff(self.incorrect_state,
+                                         self.incorrect_state.copy()))
+
+        diff_1 = bm.bitmap_diff(self.correct_state, self.incorrect_state)
+        diff_2 = bm.bitmap_diff(self.incorrect_state, self.correct_state)
+
+        self.assertIsNotNone(diff_1)
+        self.assertIsInstance(diff_1, Dict)
+
+        self.assertIsNotNone(diff_2)
+        self.assertIsInstance(diff_2, Dict)
+
+        self.assertEqual(diff_1['n_diff_pieces'], 1)
+        self.assertEqual(diff_1['larger'], 2)
+
+        self.assertEqual(diff_2['n_diff_pieces'], 1)
+        self.assertEqual(diff_2['larger'], 1)
+
+        self.assertListEqual(diff_1['first_piece'], diff_2['first_piece'])
