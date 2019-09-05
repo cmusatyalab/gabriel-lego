4b4ac59193cb191e98fe4706a992fb8bf4ca43c8
Manuel Olguin <manuel@olguin.se>
2019-09-05 18:15:49 -0400

    refactor tests

--- tests/task_test.py
+++ tests/task_test.py
@@ -17,6 +17,28 @@ class TaskTest(unittest.TestCase):
         self.logger = logging.getLogger('TestDebug')
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
+    @staticmethod
+    def show_guidance_plt(image, title='GuidanceDebug'):
+        # show guidance in Matplotlib
+        b, g, r = cv2.split(image)
+        frame_rgb = cv2.merge((r, g, b))
+        plt.imshow(frame_rgb)
+        plt.title(title)
+        plt.show()
+
+    def __update_and_check_state(self, state, index, success=True):
+        self.task.update_state(state.copy())
+        guidance = self.task.get_guidance()
+        self.logger.debug(guidance.instruction)
+        self.assertEqual(guidance.success, success)
+        self.assertEqual(index, guidance.step_id)
+
+        if success:
+            TaskTest.show_guidance_plt(guidance.image)
+        else:
+            TaskTest.show_guidance_plt(guidance.image,
+                                       title='GuidanceDebug_ExpectedError')
+
     def test_init_guidance(self):
         guidance = self.task.get_guidance()
 
@@ -24,24 +46,18 @@ class TaskTest(unittest.TestCase):
         self.assertEqual(guidance.step_id, 0)
         self.logger.debug(guidance.instruction)
 
-        # show guidance in Matplotlib
-        b, g, r = cv2.split(guidance.image)
-        frame_rgb = cv2.merge((r, g, b))
-        plt.imshow(frame_rgb)
-        plt.title('GuidanceDebug')
-        plt.show()
+        TaskTest.show_guidance_plt(guidance.image)
 
     def test_task_step_completion(self):
         for i, state in enumerate(task_Turtle.bitmaps):
-            self.task.update_state(state.copy())
-            guidance = self.task.get_guidance()
-            self.logger.debug(guidance.instruction)
-            self.assertTrue(guidance.success)
-            self.assertEqual(i + 1, guidance.step_id)
+            self.__update_and_check_state(state, i + 1)
 
-            # show guidance in Matplotlib
-            b, g, r = cv2.split(guidance.image)
-            frame_rgb = cv2.merge((r, g, b))
-            plt.imshow(frame_rgb)
-            plt.title('GuidanceDebug')
-            plt.show()
+    def test_task_step_error(self):
+        for i, state in enumerate(task_Turtle.bitmaps[:2]):
+            self.__update_and_check_state(state, i + 1)
+
+        self.__update_and_check_state(
+            state=task_Turtle.bitmaps[-1].copy(),
+            index=-1,
+            success=False
+        )
