0d422e05c2afc2b73cf6d3bdc95197178844de42
Manuel Olguin <manuel@olguin.se>
2019-09-09 10:52:38 -0400

    wrap raw bitmap in state abstraction class for Task, to handle empty boards and null states

--- lego_engine/tasks/Task.py
+++ lego_engine/tasks/Task.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import random
 import time
-from typing import NamedTuple, Optional
+from typing import Dict, NamedTuple, Optional
 
 import numpy as np
 
@@ -24,6 +24,12 @@ class BoardState:
     def __eq__(self, other: BoardState) -> bool:
         return bm.bitmap_same(self.bitmap, other.bitmap)
 
+    def empty_board(self) -> bool:
+        return False
+
+    def diff(self, other: BoardState) -> Dict:
+        return bm.bitmap_diff(self.bitmap, other.bitmap)
+
 
 class EmptyBoardState(BoardState):
     def __init__(self):
@@ -32,10 +38,39 @@ class EmptyBoardState(BoardState):
     def __eq__(self, other: BoardState) -> bool:
         return type(other) == EmptyBoardState
 
+    def empty_board(self) -> bool:
+        return True
+
+    def diff(self, other: BoardState) -> Dict:
+        raise RuntimeError('Cannot compare empty board to other states.')
+
+
+class NullBoardState(BoardState):
+    def __init__(self):
+        super().__init__(np.zeros(1))
+
+    def __eq__(self, other: BoardState) -> bool:
+        return False
+
+    def empty_board(self) -> bool:
+        raise RuntimeError('Null state error.')
+
+    def diff(self, other: BoardState) -> Dict:
+        raise RuntimeError('Cannot compare Null state to other states.')
+
+
+class NoStateChangeError(Exception):
+    pass
+
+
+class NoGuidanceError(Exception):
+    pass
+
 
 class Task:
     def __init__(self, bitmaps):
-        self.states = bitmaps
+        self.states = [BoardState(bitmap) for bitmap in bitmaps]
+        self.current_state = NullBoardState()
 
         self.prev_time = None
         self.current_time = time.time()
@@ -49,36 +84,50 @@ class Task:
         instruction = \
             "Welcome to the Lego task. As a first step, please " \
             "find a piece of 1x%d %s brick and put it on the " \
-            "board." % (target_state.shape[1],
-                        config.COLOR_ORDER[target_state[0, 0]])
+            "board." % (target_state.bitmap.shape[1],
+                        config.COLOR_ORDER[target_state.bitmap[0, 0]])
         # animation = bm.bitmap2guidance_animation(target_state,
         #                                         config.ACTION_TARGET)
-        img_guidance = bm.bitmap2guidance_img(target_state, None,
+        img_guidance = bm.bitmap2guidance_img(target_state.bitmap, None,
                                               config.ACTION_TARGET)
-        self.current_guidance = Guidance(
+        self.initial_guidance = Guidance(
             success=True,
             instruction=instruction,
             image=img_guidance,
             step_id=0
         )
 
-    def update_state(self, bitmap) -> None:
-        new_state = bitmap
+        self.current_guidance = None
+
+    def update_state(self, state: BoardState) -> None:
+        if state == self.current_state:
+            # raise exception if new state is same as old state
+            # using exceptions for flow control is Pythonic, don't @ me though
+            raise NoStateChangeError()
+
+        self.current_state = state
+
         self.prev_time = self.current_time
         self.current_time = time.time()
+
+        # if incoming new state is an empty board and target state is the
+        # first step in the task, set initial guidance
+        if state.empty_board() and self.target_state_idx == 0:
+            self.current_guidance = self.initial_guidance
+            return
 
         target_state = self.states[self.target_state_idx]
 
         # Check if we at least reached the previously desired state
-        if bm.bitmap_same(new_state, target_state):
-
+        # if bm.bitmap_same(new_state, target_state):
+        if target_state == state:
             if self.target_state_idx == (len(self.states) - 1):
                 # Task is done
                 instruction = "You have completed the task. " \
                               "Congratulations!"
                 # result['animation'] = bm.bitmap2guidance_animation(
                 #     self.current_state, config.ACTION_TARGET)
-                img_guidance = bm.bitmap2guidance_img(new_state, None,
+                img_guidance = bm.bitmap2guidance_img(state.bitmap, None,
                                                       config.ACTION_TARGET)
                 self.current_guidance = Guidance(
                     success=True,
@@ -96,7 +145,8 @@ class Task:
 
             # Determine the type of change needed for the next step
 
-            diff = bm.bitmap_diff(new_state, target_state)
+            # diff = bm.bitmap_diff(new_state, target_state)
+            diff = state.diff(target_state)
             assert diff  # states can't be the same
             assert diff['n_diff_pieces'] == 1  # for now only change one
             # piece at the time
@@ -106,8 +156,8 @@ class Task:
 
             if diff['larger'] == 2:  # target state has one more piece
                 instruction = bm.generate_message(
-                    new_state,
-                    target_state,
+                    state.bitmap,
+                    target_state.bitmap,
                     config.ACTION_ADD,
                     diff['first_piece'],
                     step_time=self.current_time - self.prev_time,
@@ -118,13 +168,13 @@ class Task:
                 #    config.ACTION_ADD,
                 #    diff_piece=diff['first_piece'])
 
-                img_guidance = bm.bitmap2guidance_img(target_state,
+                img_guidance = bm.bitmap2guidance_img(target_state.bitmap,
                                                       diff['first_piece'],
                                                       config.ACTION_ADD)
             else:  # target state has one less piece
                 instruction = bm.generate_message(
-                    new_state,
-                    target_state,
+                    state.bitmap,
+                    target_state.bitmap,
                     config.ACTION_REMOVE,
                     diff['first_piece'],
                     step_time=self.current_time - self.prev_time,
@@ -135,7 +185,7 @@ class Task:
                 #     config.ACTION_REMOVE,
                 #    diff_piece=diff['first_piece'])
 
-                img_guidance = bm.bitmap2guidance_img(new_state,
+                img_guidance = bm.bitmap2guidance_img(state.bitmap,
                                                       diff['first_piece'],
                                                       config.ACTION_REMOVE)
 
@@ -155,7 +205,7 @@ class Task:
             # result['animation'] = bm.bitmap2guidance_animation(
             #     self.prev_good_state, config.ACTION_TARGET)
             target_state = self.states[self.target_state_idx]
-            img_guidance = bm.bitmap2guidance_img(target_state,
+            img_guidance = bm.bitmap2guidance_img(target_state.bitmap,
                                                   None,
                                                   config.ACTION_TARGET)
             self.current_guidance = Guidance(
@@ -166,4 +216,6 @@ class Task:
             )
 
     def get_guidance(self) -> Guidance:
+        if self.current_guidance is None:
+            raise NoGuidanceError()
         return self.current_guidance
--- tests/task_test.py
+++ tests/task_test.py
@@ -6,9 +6,10 @@ import cv2
 import matplotlib.pyplot as plt
 
 from lego_engine.tasks import Task, task_Turtle
-
-
 # import cv2
+from lego_engine.tasks.Task import BoardState, EmptyBoardState, \
+    NoGuidanceError, \
+    NoStateChangeError
 
 
 class TaskTest(unittest.TestCase):
@@ -26,8 +27,9 @@ class TaskTest(unittest.TestCase):
         plt.title(title)
         plt.show()
 
-    def __update_and_check_state(self, state, index, success=True):
-        self.task.update_state(state.copy())
+    def __update_and_check_state(self, state: BoardState,
+                                 index: int, success=True):
+        self.task.update_state(state)
         guidance = self.task.get_guidance()
         self.logger.debug(guidance.instruction)
         self.assertEqual(guidance.success, success)
@@ -40,6 +42,12 @@ class TaskTest(unittest.TestCase):
                                        title='GuidanceDebug_ExpectedError')
 
     def test_init_guidance(self):
+        with self.assertRaises(NoGuidanceError):
+            # getting guidance without first sending a state should cause an
+            # error
+            self.task.get_guidance()
+
+        self.task.update_state(EmptyBoardState())
         guidance = self.task.get_guidance()
 
         self.assertTrue(guidance.success)
@@ -49,15 +57,35 @@ class TaskTest(unittest.TestCase):
         TaskTest.show_guidance_plt(guidance.image)
 
     def test_task_step_completion(self):
-        for i, state in enumerate(task_Turtle.bitmaps):
+        # check initial guidance using empty board
+        self.__update_and_check_state(EmptyBoardState(), 0)
+
+        for i, bitmap in enumerate(task_Turtle.bitmaps):
+            state = BoardState(bitmap.copy())
+            self.__update_and_check_state(state, i + 1)
+
+    def test_same_state_error(self):
+        # check initial guidance using empty board
+        self.__update_and_check_state(EmptyBoardState(), 0)
+
+        for i, bitmap in enumerate(task_Turtle.bitmaps[:2]):
+            state = BoardState(bitmap.copy())
+            self.__update_and_check_state(state, i + 1)
+
+        with self.assertRaises(NoStateChangeError):
+            state = BoardState(bitmap.copy())
             self.__update_and_check_state(state, i + 1)
 
     def test_task_step_error(self):
-        for i, state in enumerate(task_Turtle.bitmaps[:2]):
+        # check initial guidance using empty board
+        self.__update_and_check_state(EmptyBoardState(), 0)
+
+        for i, bitmap in enumerate(task_Turtle.bitmaps[:2]):
+            state = BoardState(bitmap.copy())
             self.__update_and_check_state(state, i + 1)
 
         self.__update_and_check_state(
-            state=task_Turtle.bitmaps[-1].copy(),
+            state=BoardState(task_Turtle.bitmaps[-1].copy()),
             index=-1,
             success=False
         )
