d6580f728f081d60138f947fa12c6b666e8dc8b5
Manuel Olguin <manuel@olguin.se>
2019-10-01 13:02:09 -0400

    refactored task states

--- gabriel_lego/lego_engine/task_manager.py
+++ gabriel_lego/lego_engine/task_manager.py
@@ -82,6 +82,21 @@ def get_error_guidance(target_state: BoardState) \
 
 
 class TaskState(ABC):
+    @property
+    @abstractmethod
+    def state_index(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def is_correct(self) -> bool:
+        pass
+
+    @property
+    @abstractmethod
+    def is_final(self) -> bool:
+        pass
+
     @staticmethod
     def generate_correct_state(task: List[BoardState], state_index: int) \
             -> Union[CorrectTaskState, FinalTaskState]:
@@ -93,15 +108,23 @@ class TaskState(ABC):
             return CorrectTaskState(task, state_index)
 
     def __init__(self):
-        self.current_image = None
-        self.current_instruction = None
+        self._current_image = None
+        self._current_instruction = None
+
+    @property
+    def current_instruction(self):
+        return self._current_instruction
+
+    @property
+    def current_image(self):
+        return self._current_image
 
     @abstractmethod
     def compute_next_task_state(self, new_board_state: BoardState) -> TaskState:
         pass
 
     def get_guidance(self):
-        return self.current_instruction, self.current_image
+        return self._current_instruction, self._current_image
 
     @abstractmethod
     def get_current_board_state(self) -> BoardState:
@@ -111,15 +134,27 @@ class TaskState(ABC):
 class InitialTaskState(TaskState):
     def __init__(self, task: List[BoardState]):
         super().__init__()
-        self.task = task
-        self.current_instruction = 'To start, put the empty LEGO board into ' \
-                                   'focus.'
+        self._task = task
+        self._current_instruction = 'To start, put the empty LEGO board into ' \
+                                    'focus.'
+
+    @property
+    def state_index(self) -> int:
+        return -1
+
+    @property
+    def is_correct(self) -> bool:
+        return True
+
+    @property
+    def is_final(self) -> bool:
+        return False
 
     def compute_next_task_state(self, new_board_state: BoardState) -> TaskState:
         if not new_board_state.empty_board:
             raise NoStateChangeError()
 
-        return CorrectTaskState(self.task, 0)
+        return CorrectTaskState(self._task, 0)
 
     def get_current_board_state(self) -> BoardState:
         raise RuntimeError('Initial task state does not have a board state.')
@@ -132,54 +167,79 @@ class CorrectTaskState(TaskState):
         super().__init__()
         assert current_state_index in range(len(task) - 1)
 
-        self.task = task
+        self._task = task
 
-        self.current_state = task[current_state_index]
+        self._current_state = task[current_state_index]
         target_state = task[current_state_index + 1]
 
-        instruction, image = get_guidance_between_states(self.current_state,
+        instruction, image = get_guidance_between_states(self._current_state,
                                                          target_state)
 
-        self.current_state_index = current_state_index
-        self.current_instruction = instruction
-        self.current_image = image
+        self._current_state_index = current_state_index
+        self._current_instruction = instruction
+        self._current_image = image
+
+    @property
+    def state_index(self) -> int:
+        return self._current_state_index
+
+    @property
+    def is_correct(self) -> bool:
+        return True
+
+    @property
+    def is_final(self) -> bool:
+        return False
 
     def compute_next_task_state(self, new_board_state: BoardState) -> TaskState:
-        if self.current_state == new_board_state:
+        if self._current_state == new_board_state:
             raise NoStateChangeError()
 
-        target_state_index = self.current_state_index + 1
-        target_state = self.task[target_state_index]
+        target_state_index = self._current_state_index + 1
+        target_state = self._task[target_state_index]
         # Check if we reached the next state
         if target_state == new_board_state:
             # Next state is simply the next one in line
-            return TaskState.generate_correct_state(self.task,
+            return TaskState.generate_correct_state(self._task,
                                                     target_state_index)
 
         else:
             return IncorrectTaskState(
-                task=self.task,
-                target_state_index=self.current_state_index,
+                task=self._task,
+                target_state_index=self._current_state_index,
                 current_state=new_board_state)
 
     def get_current_board_state(self) -> BoardState:
-        return self.current_state
+        return self._current_state
 
 
 class FinalTaskState(TaskState):
     def __init__(self, task: List[BoardState]):
         super().__init__()
-        self.task = task
-        self.current_instruction = "You have completed the task. " \
-                                   "Congratulations!"
-        self.current_image = bm.bitmap2guidance_img(task[-1].bitmap, None,
-                                                    config.ACTION_TARGET)
+        self._task = task
+        self._current_state_index = len(task) - 1
+        self._current_instruction = "You have completed the task. " \
+                                    "Congratulations!"
+        self._current_image = bm.bitmap2guidance_img(task[-1].bitmap, None,
+                                                     config.ACTION_TARGET)
+
+    @property
+    def state_index(self) -> int:
+        return self._current_state_index
+
+    @property
+    def is_correct(self) -> bool:
+        return True
+
+    @property
+    def is_final(self) -> bool:
+        return True
 
     def compute_next_task_state(self, new_board_state: BoardState) -> TaskState:
         raise NoStateChangeError()
 
     def get_current_board_state(self) -> BoardState:
-        return self.task[-1]
+        return self._task[-1]
 
 
 class IncorrectTaskState(TaskState):
@@ -190,46 +250,58 @@ class IncorrectTaskState(TaskState):
         super().__init__()
         assert target_state_index in range(len(task))
 
-        self.target_state_index = target_state_index
-        self.target_state = task[target_state_index]
+        self._target_state_index = target_state_index
+        self._target_state = task[target_state_index]
 
-        instruction, image = get_error_guidance(self.target_state)
+        instruction, image = get_error_guidance(self._target_state)
 
-        self.task = task
-        self.current_state = current_state
-        self.current_instruction = instruction
-        self.current_image = image
+        self._task = task
+        self._current_state = current_state
+        self._current_instruction = instruction
+        self._current_image = image
+
+    @property
+    def state_index(self) -> int:
+        return -1
+
+    @property
+    def is_correct(self) -> bool:
+        return False
+
+    @property
+    def is_final(self) -> bool:
+        return False
 
     def compute_next_task_state(self, new_board_state: BoardState) -> TaskState:
-        if new_board_state == self.current_state:
+        if new_board_state == self._current_state:
             raise NoStateChangeError()
 
-        if new_board_state == self.target_state:
+        if new_board_state == self._target_state:
             # fixed error
             return CorrectTaskState(
-                task=self.task,
-                current_state_index=self.target_state_index)
+                task=self._task,
+                current_state_index=self._target_state_index)
 
         else:
             return IncorrectTaskState(
-                task=self.task,
-                target_state_index=self.target_state_index,
+                task=self._task,
+                target_state_index=self._target_state_index,
                 current_state=new_board_state)
 
     def get_current_board_state(self) -> BoardState:
-        return self.current_state
+        return self._current_state
 
 
 class TaskManager:
     def __init__(self):
-        self.tasks = {
+        self._tasks = {
             name: [EmptyBoardState()] + [BoardState(b) for b in bitmaps]
             for name, bitmaps in tasks.task_collection.items()
         }
 
     def _get_task(self, task_name: str) -> List[BoardState]:
         try:
-            return self.tasks[task_name]
+            return self._tasks[task_name]
         except KeyError:
             raise NoSuchTaskError(task_name)
 
