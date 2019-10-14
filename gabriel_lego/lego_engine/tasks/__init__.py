229b51a0bea98a27af92d9350ed4897b7b486519
Manuel Olguin <manuel@olguin.se>
2019-10-14 14:58:20 -0400

    add generated tasks

--- gabriel_lego/lego_engine/tasks/__init__.py
+++ gabriel_lego/lego_engine/tasks/__init__.py
@@ -2,8 +2,12 @@ from typing import Dict, List
 
 import numpy as np
 
-from . import task_Turtle as __task_turtle
+from . import task_Turtle as __task_turtle, task_generated_20 as __task_20, \
+    task_generated_45 as __task_45, task_generated_90 as __task_90
 
 task_collection: Dict[str, List[np.ndarray]] = {
-    'turtle_head': __task_turtle.bitmaps
+    'turtle_head' : __task_turtle.bitmaps,
+    'generated_20': __task_20,
+    'generated_45': __task_45,
+    'generated_90': __task_90
 }
